3ccb0f2f699932e38a7739fc9e6bfa88093e0fc3
Grigore <grigore.crudu@logifect.com>
2024-12-09 14:31:19 +0200

    Initialize project my portofolio

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+export default function Projects() {
+	return (
+		<main>
+			<h1>My Projects</h1>
+			<ul>
+				<li>Portfolio Website</li>
+				<li>E-commerce App</li>
+			</ul>
+		</main>
+	);
+}
